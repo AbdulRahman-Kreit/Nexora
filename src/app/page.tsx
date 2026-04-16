dc58fb887ae7544e15d939d45d868fb32dab5e42
AbdulRahman-Kreit <aleppianeren@gmail.com>
2026-04-16 13:33:57 +0200

    Starting Page has been added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,80 @@
+"use client"; 
 import Image from "next/image";
-import Link from "next/link";
+import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleAnimationComplete = () => {
+    setTimeout(() => {
+      router.push("/login");
+    }, 2000); 
+  }
+
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-center 
-    p-24`}>
-      <div className="flex flex-col items-center gap-2 mb-12 w-[350px] text-center">
-        <Image 
-          src='/assits/image_8.png'
-          alt="A door image"
-          width={300}
-          height={300}
-          priority
-        />
-        <h2 className="text-4xl mt-0 mb-6 font-semibold">
-          Sign in or create a new account
-        </h2>
-      </div>
-      <div className="flex gap-x-5">
-        <Link className={`flex justify-center items-center gap-2 py-5 px-10 border 
-        border-[#2d2d2d] rounded-md font-semibold hover:bg-[#1e1e1e] 
-        transition duration-200 ease-in-out w-[300px]`}
-        href="#">
-          <img src="/assits/tabler-icon-brand-google.png" alt="Google icon" />
-          Continue with Google
-        </Link>
-        <Link className={`flex justify-center items-center gap-2 py-5 px-10 border 
-        border-[#2d2d2d] rounded-md font-semibold hover:bg-[#1e1e1e] 
-        transition duration-200 ease-in-out w-[300px]`}
-        href="#">
-          <img src="/assits/tabler-icon-brand-apple.png" alt="Apple icon" />
-          Continue with Apple
-        </Link>
-        <Link className={`flex justify-center items-center gap-2 py-5 px-10 border 
-        border-[#2d2d2d] rounded-md font-semibold hover:bg-[#1e1e1e] 
-        transition duration-200 ease-in-out w-[300px]`}
-        href="#">
-          <img src="/assits/tabler-icon-brand-windows.png" alt="Microsoft icon" />
-          Continue with Microsoft
-        </Link>
-      </div>
-      <p className="text-sm text-center mt-6 opacity-50">
-        By continuing, you agree to our <Link className="underline" href='#'>Terms of Service</Link>, <Link className="underline" href='#'>Privacy Policy</Link> and <Link className="underline" href='#'>Data Protection Agreement</Link>
-      </p>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-[#1a1a1a] p-24 overflow-hidden">
+      <motion.div 
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        className="flex flex-col items-center w-fit"
+      >
+        <div className="flex flex-row relative pr-[230px]">
+          <motion.div
+            initial={{ x: -30, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.6, ease: "easeOut" }}
+            className="relative z-10" 
+          >
+            <Image 
+              src='/assits/Asset_5.svg'
+              alt="Nex part"
+              width={300}
+              height={300}
+              priority
+              className="h-auto"
+            />
+          </motion.div>
+
+          <motion.div 
+            initial={{ x: -100, opacity: 0 }} 
+            animate={{ x: 0, opacity: 1 }}    
+            transition={{ 
+              delay: 0.5, 
+              duration: 0.8, 
+              ease: "circOut" 
+            }}
+            className="absolute bottom-1 left-[270px] z-0"
+          >
+            <Image 
+              src='/assits/Asset_7.svg'
+              alt="ora part"
+              width={260}
+              height={50}
+              priority
+              className="h-auto"
+            />
+          </motion.div>
+        </div>
+
+        <motion.div 
+          initial={{ y: -15, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 1.2, duration: 0.5 }}
+          onAnimationComplete={handleAnimationComplete}
+          className="mt-4 ml-[250px]"
+        >
+          <Image 
+            src='/assits/Asset_6.svg'
+            alt="Analytics part"
+            width={280}
+            height={300}
+            priority
+            className="h-auto"
+          />
+        </motion.div>
+      </motion.div>
     </main>
   );
 }
